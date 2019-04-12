2eff942535b659923ae39bccbd8e3369bcfe092b
khaoula-abaidi <abaidik@gmail.com>
2019-04-12 16:59:20 +0200

    Security Form Changement

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -26,14 +27,20 @@ class User
     /**
      * @ORM\Column(type="string", length=255)
      */
+    private $pwd;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $email;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Decision", mappedBy="user")
      */
     private $decisions;
-
-
+    /**
+     * @var array @ORM\Column(type="json")
+     */
+ private $roles =[];
 
     public function __construct()
     {
@@ -45,16 +52,13 @@ class User
         return $this->id;
     }
 
+    /**
+     * @see UserInterface
+     * @return string|null
+     */
     public function getUsername(): ?string
     {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
+        return $this->email;
     }
 
     public function getEmail(): ?string
@@ -98,5 +102,67 @@ class User
         }
 
         return $this;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return ['ROLE_USER'];
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @see UserInterface
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     * @see UserInterface
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @see UserInterface
+     * @return string|null The salt
+     */
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
     }
 }
--- src/Security/Voter/UserVoter.php
+++ src/Security/Voter/UserVoter.php
@@ -11,11 +11,12 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class UserVoter extends Voter
 {
     const VIEW = 'view';
+    const DELETE = 'delete';
     protected function supports($attribute, $subject)
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::VIEW])
+        return in_array($attribute, [self::VIEW,self::DELETE])
             && $subject instanceof \App\Entity\Decision;
     }
 
@@ -44,13 +45,15 @@ class UserVoter extends Voter
                 break;
 
         }
-          throw new \LogicException('accès interdit .....');
-        //return false;
+          //throw new \LogicException('accès interdit .....');
+        return false;
     }
+    /*
     private function canView(Decision $decision,User $user){
         if($this->canView($decision,$user)){
             return true;
         }
         return $decision->getContent();
     }
+    */
 }
